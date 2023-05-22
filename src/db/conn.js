cc2dca35ca289762f14270405e88c6f378b03c71
Aman <aman99.er@gmail.com>
2023-05-22 17:57:50 +0530

    changes made

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-// const DB = process.env.DATABASE;
-const DB = "mongodb+srv://amanverma48003:nAmA%239211MDB@cluster0.mhqjntd.mongodb.net/Mydatabase?retryWrites=true&w=majority";
+const DB = process.env.DATABASE;
+// const DB = "mongodb+srv://amanverma48003:nAmA%239211MDB@cluster0.mhqjntd.mongodb.net/Mydatabase?retryWrites=true&w=majority";
 
 mongoose.connect(DB)
     .then(() => console.log("Connection Successful with Database 'registrationform' "))
